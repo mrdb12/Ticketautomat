c3215811d387b2103320375d542f99222e8ca6bc
mrdb12 <dbruchwald@gmail.com>
2021-10-10 19:28:35 +0200

    You can choose a ticket and it is printed out

--- main.c
+++ main.c
@@ -4,28 +4,39 @@
 
 void runMenu();
 struct ticket *ini24HTicket();
+struct ticket *iniOneTimeTicket();
+void printTicket(struct ticket *ticket);
+
 
 struct ticket{
   char name[15];
   double price;
-
 };
 
 int main(void){
-
   runMenu();
-
 
   return 0;
 }
 
 void runMenu(){
   char option;
+  struct ticket *ticket;
 
   printf("Do you want to buy a ticket(1) or quit(2)\n");
   scanf("%c", &option);
   if(option == '1'){
-    printf("Ticket choosen");
+    printf("Which ticket do you want to buy?:\n24HTicket (1)\nOneTimeTicket (2)\n");
+    scanf("\n%c", &option);
+    if (option == '1') {
+      ticket = ini24HTicket();
+      printTicket(ticket);
+    }else if (option == '2') {
+      ticket = iniOneTimeTicket();
+      printTicket(ticket);
+    }else{
+      printf("Illegal input\n");
+    }
   }else if(option == '2'){
     printf("Quiting");
   }else{
@@ -41,4 +52,20 @@ struct ticket *ini24HTicket(){
   ptrTicket->price = 8.8;
 
   return ptrTicket;
+}
+
+struct ticket *iniOneTimeTicket(){
+  struct ticket *ptrTicket;
+  ptrTicket = (struct ticket *) malloc(sizeof(struct ticket));
+  strcpy(ptrTicket->name, "OneTimeTicket");
+  ptrTicket->price = 3.3;
+
+  return ptrTicket;
+}
+
+void printTicket(struct ticket *ticket){
+  printf("....................\n");
+  printf("You bought the ticket: %s\n", ticket->name);
+  printf("For %.2f $\n", ticket->price);
+  printf("....................\n");
 }
